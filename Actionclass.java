e02dfea3b02c72eacd5b375ffe1155410c92fdb5
shivani-jagtap <150342616+shivani-jagtap@users.noreply.github.com>
2023-12-18 13:10:15 +0530

    Add files via upload

---
+++ Actionclass.java
@@ -0,0 +1,29 @@
+package Assigenment3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Actionclass {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
+
+        driver.findElement(By.xpath("//textarea[@id='APjFqb']")).sendKeys("selenium");
+
+
+        Actions action = new Actions(driver);
+        action.sendKeys(Keys.ENTER).build().perform();
+        driver.close();
+    }
+}
